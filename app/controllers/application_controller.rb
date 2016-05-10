8edc87cea38f9628b28b511a7f240880ecf0f0b5
j_victoria <jvictor5@binghamton.edu>
2016-05-10 19:19:09 +0000

    added some little things

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def index
+    #if ! cookies[:email].blank? and cookes[:login].blank?
+    #  if cookies[:login] == User.find_by(email: cookies[:email]).id
+    #    session[:user] = User.find(cookies[:login])
+    #    redirect_to(user_profile_path)
+    #  end
+    #end
     @buses = Business.all
     @users = User.all
   end
