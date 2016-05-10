145e7f7da322b0872e5fcc08d94dc58849abbed6
j-victoria <jvictoria1203@gmail.com>
2016-05-10 11:10:58 -0400

    Create schema.rb

---
+++ db/schema.rb
@@ -0,0 +1,52 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20160423213250) do
+
+  create_table "businesses", force: :cascade do |t|
+    t.string "name"
+    t.float  "lat"
+    t.float  "lng"
+    t.float  "average"
+    t.string "address"
+  end
+
+  create_table "businesses_types", id: false, force: :cascade do |t|
+    t.integer "type_id",     null: false
+    t.integer "business_id", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "stars"
+    t.text     "description"
+    t.integer  "user_id"
+    t.integer  "business_id"
+    t.datetime "create_date"
+  end
+
+  create_table "types", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.string   "name"
+  end
+
+end
